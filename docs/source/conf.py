07745207f564b618b2e1874e6c069380d9d2fc90
Doggy Sasha <sasha.hannarom@ya.ru>
2024-06-23 22:50:08 +0300

    Изменение конфига RTD (3)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,57 +9,55 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
-import subprocess, os
-import sys
-
-sys.path.insert(0, os.path.abspath('../..'))
-
-# Generate the XML files for Sphinx
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
-if read_the_docs_build:
-    curr_dir = os.path.abspath('.')
-    os.chdir('..')
-    subprocess.call('doxygen', shell=True)
-    os.chdir(curr_dir)
 
 # -- Project information -----------------------------------------------------
 
-project = 'xengine-docs'
-copyright = '2023, Xanytka Devs'
-author = 'Xanytka Devs'
+project = "Firesteel Docs"
+copyright = "2023, Xanytka Devs"
+author = "Xanytka Devs"
 
-# The full version, including alpha/beta/rc tags
-release = '0.1'
 
 # -- General configuration ---------------------------------------------------
+# -- General configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ['breathe']
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+templates_path = ["_templates"]
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-breathe_projects = {
-    "cpp_project": "../build/xml"
-}
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
