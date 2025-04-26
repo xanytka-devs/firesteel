9e61901b1e6122ee853006ece6eaa5943566c37a
Sanya Alabai <70968297+sanyaalabai@users.noreply.github.com>
2025-04-26 14:25:33 +0300

    Точно последний багфикс*
    * - не обещаю.

--- include/entity.hpp
+++ include/entity.hpp
@@ -25,7 +25,7 @@ namespace Firesteel {
         std::vector<Mesh> meshes;
         std::string path;
         std::string getDirectory() const {
-            return path.substr(0, path.find_last_of('\\')));
+            return path.substr(0, path.find_last_of('\\'));
         }
         Model(const std::string& tPath = "") {
             textures.clear();
